7c95640e010dc9cad3f34ee8e9efd014332d2cda
William Serna <williamsernac@hotmail.com>
2022-07-24 11:30:20 -0500

    Update and enable cors handling

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const launchShipRoute = require('./routes/shipLaunchRoutes');
 const unmannedShipRoute = require('./routes/unmannedShipRoutes');
@@ -10,12 +11,7 @@ require("./config/db");
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use((res, req, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.header("Access-Control-Headers", "Origin, X-Requested-Width, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next()
-});
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(launchShipRoute);
